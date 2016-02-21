246ed0f10826044240139a9cbc618f31ff8b40e0
GHDmitro <d.timosh1996@gmail.comd.timosh1996@gmail.comd.timosh1996@gmail.com>
2016-02-21 19:29:40 +0200

    Rejected commit: Default

---
+++ homeWork3/ControlCash.java
@@ -0,0 +1,18 @@
+package homeWork3;
+
+/**
+ * Created by macbookair on 21.02.16.
+ */
+public class ControlCash {
+
+    public static void main(String[] args) throws InterruptedException {
+        String pathOfPage = "/Users/macbookair/IdeaProjects/JavaPro/src/homeWork3";
+
+        FileManager fm = new FileManager(pathOfPage);
+        for (int i = 0; i < 5; i++) {
+            fm.get("/index.html");
+            Thread.sleep(3000);
+        }
+    }
+
+}
--- homeWork3/FileManager.java
+++ homeWork3/FileManager.java
@@ -1,42 +1,33 @@
-package lesson3;
+package homeWork3;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 //
 public class FileManager {
-//    private long firstTime;
-//    private long lastTime;
 
-
-    private long time = 3000;
+    private int number = 1;
+    private long time = 2000;
     private String path;
     //потобезопастный hashmap
     //key = url , [] значения
     private static ConcurrentHashMap<String, byte[]> map = new ConcurrentHashMap<String, byte[]>();
 
     public FileManager(String path) {
-        // "c:\folder\" --> "c:\folder"
+//        // "c:\folder\" --> "c:\folder"
         if (path.endsWith("/") || path.endsWith("\\"))
             path = path.substring(0, path.length() - 1);
 
-        this.path = path;
-    }
-
-    public FileManager(String path, long mill) {
-        if (path.endsWith("/") || path.endsWith("\\"))
-            path = path.substring(0, path.length() - 1);
 
         this.path = path;
-        this.time = time;
     }
 
     public byte[] get(String url) {
-//        firstTime = (long) System.currentTimeMillis();
+
         try {
             byte[] buf = map.get(url);
             if (buf != null) {// in cache
-//                lastTime = System.currentTimeMillis();
                 return buf;
             }
 
@@ -57,8 +48,8 @@ public class FileManager {
 
 //------------Task 1 : Control time life of url objects in cash --------------------------
             final byte[] finalBuf = buf;
-            System.out.println(System.currentTimeMillis());
-            System.out.println("Size url while alive : " + FileManager.map.size());
+            System.out.println("\nTime when alive " + number + " : " + System.currentTimeMillis());
+            System.out.println("Size map while alive  : " + FileManager.map.size());
             Thread t = new Thread(() -> {
                 try {
                     Thread.currentThread();
@@ -67,16 +58,15 @@ public class FileManager {
                     e.printStackTrace();
                 }
                 synchronized (this) {
-                    FileManager.map.remove(url);
-                    System.out.println("Size url while die : " + FileManager.map.size());
-                    System.out.println(System.currentTimeMillis());
+                    System.out.println("\nRemoving object " + number + " : " + FileManager.map.remove(url).toString());
+                    System.out.println("Size map while die  : " + FileManager.map.size());
+                    System.out.println("Time after removing  : " + System.currentTimeMillis());
                 }
                 Thread.currentThread().interrupt();
             });
             t.start();
-            System.out.println(buf);
 //-----------------------------------------------------------------------------------------
-
+            number++;
             return buf;
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -84,5 +74,4 @@ public class FileManager {
         }
     }
 
-//    private void handler()
 }
